d322baa587a6a58ab7d3670621ee1969aad3fb22
HaebinShin <sunsal0704@gmail.com>
2016-05-30 09:10:21 +0900

    Add basic parser and tokenizer class

---
+++ parser.py
@@ -0,0 +1,113 @@
+import re
+
+class Parser:
+	def __init__(self):
+		self.tree=[]
+
+	def __str__(self):
+		return "%s" % self.tree
+	
+	def __repr__(self):
+		return "%s" % self.tree
+	
+	def parse(self, tokens):
+		self.tree=self.takeExpression(tokens)
+		return self.tree
+	
+	def takeExpression(self, tokens):
+		terms = []
+		ops = []
+		terms.append(self.takeTerm(tokens))
+		while tokens.isEmpty()==False and tokens.front() in ['+','-']:
+			ops.append(tokens.front())
+			tokens.popFront()
+			terms.append(self.takeTerm(tokens))
+		return Expression(terms, ops)
+
+	def takeTerm(self, tokens):
+		factors = []
+		ops = []
+		factors.append(self.takeFactor(tokens))
+		while tokens.isEmpty()==False and tokens.front() in ['*', '/']:
+			ops.append(tokens.front())
+			tokens.popFront()
+			factors.append(self.takeFactor(tokens))
+		return Term(factors, ops)
+
+	def takeFactor(self, tokens):
+		now = tokens.front()
+		if now == '(':
+			tokens.popFront()
+			exp = self.takeExpression(tokens)
+			tokens.popFront()
+			return Factor(['(',exp,')'],"paran")
+		elif now == '-':
+			tokens.popFront()
+			fac = tokens.front()
+			tokens.popFront()
+			return Factor(['-',fac], "negative")
+		elif re.match("\d*\.\d+|\d+", now)!=None:
+			tokens.popFront()
+			return Factor(now, "number")
+		else:
+			tokens.popFront()
+			return Factor(now, "symbol")
+
+	
+class Factor:
+	def __init__(self, param, typename):
+		self.factors=[]
+		if type(param)==list:
+			for i in param:
+				self.factors.append(i)
+		else:
+			self.factors.append(param)
+
+	def __str__(self):
+		return "%s" % (self.factors)
+	
+	def __repr__(self):
+		return "%s" % (self.factors)
+
+
+class Term:
+	def __init__(self, factors, ops):
+		self.terms=[]
+		self.terms.append(factors[0])
+		for i in range(len(ops)):
+			self.terms.append(ops[i])
+			self.terms.append(factors[i+1])
+
+	def __str__(self):
+		return "%s" % (self.terms)
+
+	def __repr__(self):
+		return "%s" % (self.terms)
+
+
+class Expression:
+	def __init__(self, terms, ops):
+		self.expressions=[]
+		self.ops=[]
+		self.expressions.append(terms[0])
+		for i in range(len(ops)):
+			self.expressions.append(ops[i])
+			self.expressions.append(terms[i+1])
+
+	def __str__(self):
+		return "%s" % (self.expressions)
+
+	def __repr__(self):
+		return "%s" % (self.expressions)
+
+
+if __name__ == "__main__":
+
+	from tokenizer import Tokenizer
+
+	tker=Tokenizer()
+	tokens=tker.tokenize("x*(y+2)")
+	print tokens
+	p=Parser()
+	p.parse(tokens)
+	print p
---
+++ tokenizer.py
@@ -0,0 +1,46 @@
+import re
+
+class Tokens:
+	def __init__(self, tokens):
+		self.arr=tokens
+
+	def __str__(self):
+		return "%s" % (self.arr)
+
+	def __repr__(self):
+		return "%s" % (self.arr)
+
+	def popFront(self):
+		return self.arr.pop(0)
+
+	def front(self):
+		return self.arr[0]
+
+	def size(self):
+		return len(self.arr)
+
+	def isEmpty(self):
+		if self.size()==0:
+			return True
+		else:
+			return False
+	
+
+class Tokenizer:
+
+	def __init__(self):
+		self.tokens=[]
+
+	def tokenize(self, input_string):
+		input_string=input_string.replace(" ", "")
+		self.tokens=re.findall("[\+\-\*\/\(\)]|\d*\.\d+\d+|sin|cos|tan|log|exp|\w+", input_string)
+		return Tokens(self.tokens)
+
+
+
+if __name__ == "__main__":
+	tker=Tokenizer()
+	tokens=tker.tokenize("xy+sin(x+cos(x))-(x+x)")
+	print tokens
+	tokens=tker.tokenize("2x")
+	print tokens
